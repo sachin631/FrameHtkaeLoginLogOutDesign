22273dd6aa3f87b1448ea20242ea5ac032ecadcc
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2022-10-13 01:04:20 +0530

    added dependency from old setup
    - installed tailwind
    - installed react router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = '리액트';
-  console.log(name);
   return (
     <div className="App">
       <header className="App-header">
